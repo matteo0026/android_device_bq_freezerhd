449544fae11ff86f0c3573277edb2bb627514973
matteo0026 <marmotta200026@gmail.com>
2019-09-29 16:49:50 +0200

    Update to android-8.1

--- omni_freezerhd.mk
+++ omni_freezerhd.mk
@@ -1,9 +1,19 @@
-# Inherit device configuration
-$(call inherit-product, device/bq/freezerhd/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/bq/freezerhd/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += device/bq/freezerhd/kernel:kernel
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := freezerhd
 PRODUCT_NAME := omni_freezerhd
 PRODUCT_BRAND := bq
-PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M10
+PRODUCT_MANUFACTURER := bq
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=freezerhd
+
